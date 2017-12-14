e2c50f5b0d8b21bb9f51e1f4024bcc774f0fd61a
willberman1004 <wberman@stuy.edu>
2017-12-13 21:55:26 -0500

    v3 commit

---
+++ UserOfRPGv2.java
@@ -0,0 +1,21 @@
+public class UserOfRPGv2 {
+    
+    public static Protagonist steven = new Protagonist("steven");
+
+    public static Archer artemis = new Archer("artemis");
+
+    public static Warrior joan = new Warrior("joan");
+
+    public static Mage harry = new Mage("harry");
+
+    public static Monster cthulu = new Monster("cthulu");
+
+    public static Slime jim = new Slime("jim");
+
+    public static Bandit billy = new Bandit("billy");
+
+    public static Dragon paarthurnax = new Dragon("paarthurnax");
+
+    public static Character[] agents;
+    
+}
